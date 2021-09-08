5c146ba2021d8f52336020cbc343e0b1730c46b1
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2021-09-08 14:28:41 -0500

    preparing heroku deploy

--- src/main/java/edu/escuelaing/arep/weatherapi/httpserver/WebServer.java
+++ src/main/java/edu/escuelaing/arep/weatherapi/httpserver/WebServer.java
@@ -3,9 +3,15 @@
  */
 package edu.escuelaing.arep.weatherapi.httpserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
@@ -65,7 +71,89 @@ public class WebServer {
 	 * @throws URISyntaxException - Cuando no es posible interpretar la URI.
 	 */
 	public void serverConnection(Socket clientSocket) throws IOException, URISyntaxException {
+		if (clientSocket != null) {
 
+			PrintWriter out;
+			BufferedReader in;
+			OutputStream los_outputStream;
+			InputStream lis_inputStream;
+
+			los_outputStream = clientSocket.getOutputStream();
+			lis_inputStream = clientSocket.getInputStream();
+
+			if ((los_outputStream != null) && (lis_inputStream != null)) {
+
+				String inputLine, outputLine;
+				StringBuilder request;
+				InputStreamReader lisr_inputStreamReader;
+
+				request = new StringBuilder();
+				lisr_inputStreamReader = new InputStreamReader(lis_inputStream);
+				out = new PrintWriter(los_outputStream, true); // envío de msgs al Cliente.
+
+				if (lisr_inputStreamReader != null) {
+					in = new BufferedReader(lisr_inputStreamReader); // recibir msgs del Cliente
+
+					if (in != null && in.ready()) {
+
+						while ((inputLine = in.readLine()) != null) {
+							// System.out.println("Received: " + inputLine);
+							request.append(inputLine);
+							if (!in.ready()) {
+								break;
+							}
+						}
+					}
+					String ls_request;
+					ls_request = request.toString();
+
+					if ((ls_request != null) && (!ls_request.isEmpty())) {
+
+						String ls_uriStr;
+						String[] las_request;
+
+						las_request = ls_request.split(" ");
+
+						if ((las_request != null)) {
+							ls_uriStr = las_request[1];
+
+							if ((ls_uriStr != null) && (!ls_uriStr.isEmpty())) {
+								URI resourceURI;
+								resourceURI = new URI(ls_uriStr);
+
+								if (ls_uriStr.equals("/")) {
+									
+										outputLine = default404Response();
+										out.println(outputLine);
+								} else {
+									throw new IOException("ServerConnection ls_uriStr desconocida!");
+								}
+							}
+						}
+					}
+					out.close();
+					in.close();
+				}
+			} else {
+				throw new IOException("ServerConnection BufferReader input vacío o nulo!");
+			}
+			clientSocket.close();
+		} else {
+			throw new IOException("ServerConnection Socket no puede ser nulo");
+		}
+	}
+	
+
+	/**
+	 * Página por defecto al intentar conectar con el servidor
+	 * 
+	 * @return la página por defecto en html
+	 */
+	private String default404Response() {
+		String outputLine = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + "\r\n" + "<!DOCTYPE html>\n"
+				+ "<html>\n" + "	<head>\n" + "		<meta charset=\"UTF-8\">\n" + "		<title>Inicio</title>\n"
+				+ "	</head>\n" + "	<body>\n" + "		<h1>NOT FOUND 404</h1>\n" + "	</body>\n" + "</html>\n";
+		return outputLine;
 	}
 
 }
